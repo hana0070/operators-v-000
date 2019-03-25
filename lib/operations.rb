bfed0cee4c991c1aa05202b6d4f9563e36916325
HANA LEANDER <hana0070@gmail.com>
2019-03-25 19:40:37 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,13 +1,9 @@
 def unsafe?(speed)
-	if speed > 60 ? unsafe : safe
+	if speed > 60 ? true : false
 end
 
 
-#describe "#unsafe?" do
-	it 'returns true if speed is greater than 60' do
-		answer = unsafe?(79)
-		expect(answer).to eq(true)
-	end
+
 
 
 def not_safe?(speed)
